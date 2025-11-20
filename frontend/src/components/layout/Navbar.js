1fab8a5a4d3edb01d2ad2356408752ecb8c84cc8
Alan Zhang <alan.s.zhang@gmail.com>
2025-11-20 12:54:22 -0500

    navbar footer styling logic

--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
+import styles from "./Navbar.module.scss";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <header className={styles.navbar}>
+      <div className={styles.inner}>
+        <Link href="/" className={styles.brand}>
+          <span className={styles.logoCircle}>YS</span>
+          <span className={styles.brandText}>YSC Lunch Soccer</span>
+        </Link>
 
-export default Navbar
+        <nav className={styles.navLinks} aria-label="Main navigation">
+          <Link href="/" className={styles.link}>
+            Home
+          </Link>
+          <Link href="/" className={styles.link}>
+            Sessions
+          </Link>
+        </nav>
+
+        <div className={styles.authLinks}>
+          <Link href="/login" className={styles.loginLink}>
+            Log In
+          </Link>
+          <Link href="/signup" className={styles.signupButton}>
+            Sign Up
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
+
+
