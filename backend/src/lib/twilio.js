126c8ec3ca347765c16dc71fb529e0ff8c879e8b
alanzhang03 <107202923+alanzhang03@users.noreply.github.com>
2026-03-10 20:35:02 -0400

    deleted testing file, removed console logs

--- backend/src/lib/twilio.js
+++ backend/src/lib/twilio.js
@@ -6,19 +6,14 @@ const authToken = process.env.TWILIO_AUTH_TOKEN;
 const twilioNum = process.env.TWILIO_PHONE_NUMBER;
 const client = twilio(accountSid, authToken);
 
-
-//Thinking about passing in info like [(name, phoneNumber)],
 async function sendSms(to, body) {
   for (let i = 0; i < to.length; i++) {
     const message = await client.messages.create({
       body: body,
       from: twilioNum,
-      to: to[i].phone//assuming i[1] is number and i[0] is user's name
+      to: to[i].phone,
     });
-
-    console.log(message.body)
   }
 }
 
-export default sendSms
-
+export default sendSms;
