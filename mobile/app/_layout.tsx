dd24fcc183b10f41c5011505354174fd699e7559
alanzhang03 <107202923+alanzhang03@users.noreply.github.com>
2026-03-13 11:05:11 -0400

    login buttons etc

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,7 +1,12 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
 import { AuthProvider } from '@/contexts/AuthContext';
@@ -14,14 +19,21 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <AuthProvider>
-      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-        </Stack>
-        <StatusBar style="auto" />
-      </ThemeProvider>
-    </AuthProvider>
+    <SafeAreaProvider>
+      <AuthProvider>
+        <ThemeProvider
+          value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}
+        >
+          <Stack>
+            <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+            <Stack.Screen
+              name='modal'
+              options={{ presentation: 'modal', title: 'Modal' }}
+            />
+          </Stack>
+          <StatusBar style='auto' />
+        </ThemeProvider>
+      </AuthProvider>
+    </SafeAreaProvider>
   );
 }
--- mobile/app/login.tsx
+++ mobile/app/login.tsx
@@ -1,12 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 import React from 'react';
 import { colors } from '@/constants/styles';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useState } from 'react';
 
 const login = () => {
+  const [identifier, setIdentifier] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <View style={styles.container}>
-      <Text>login</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.infoContainer}>
+        <Text>login</Text>
+        <TextInput
+          placeholder='Email or Phone Number'
+          style={styles.input}
+          onChangeText={setIdentifier}
+          value={identifier}
+        />
+        <TextInput
+          placeholder='Password'
+          style={styles.input}
+          onChangeText={setPassword}
+          value={password}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
@@ -16,5 +35,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.background,
+  },
+  infoContainer: {
+    borderRadius: 8,
+  },
+  input: {
+    borderRadius: 8,
   },
 });
